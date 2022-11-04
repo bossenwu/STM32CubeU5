bf9c179f4d75814d3db9973a8771862c22504a8b
bossenwu <bossen.wu@st.com>
2022-11-04 16:42:16 +0800

    NS appli: add interface to change password

--- Projects/NUCLEO-U575ZI-Q/Applications/TFM/TFM_Appli/NonSecure/Src/user_app.c
+++ Projects/NUCLEO-U575ZI-Q/Applications/TFM/TFM_Appli/NonSecure/Src/user_app.c
@@ -1,20 +1,20 @@
 /**
-  ******************************************************************************
-  * @file    user_app.c
-  * @author  Bossen WU
-  * @brief   This file provides example of user app access.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    user_app.c
+ * @author  Bossen WU
+ * @brief   This file provides example of user app access.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 
 /* Includes ------------------------------------------------------------------*/
 #include <string.h>
@@ -23,35 +23,34 @@
 #include "com.h"
 #include "psa_manifest/sid.h"
 
-
-
 /** @defgroup  user_app_Private_Defines Private Defines
-  * @{
-  */
+ * @{
+ */
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /** @defgroup  user_app_Private_Functions Private Functions
-  * @{
-  */
+ * @{
+ */
 static void user_app_print_menu(void);
-static psa_status_t user_app_action(void);
+static psa_status_t user_app_io_action(void);
+static psa_status_t user_app_chgpwd(void);
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /** @defgroup  user_app_Exported_Functions Exported Functions
-  * @{
-  */
+ * @{
+ */
 
 /**
-  * @brief  Display the user app Main Menu choices on HyperTerminal
-  * @param  None.
-  * @retval None.
-  */
+ * @brief  Display the user app Main Menu choices on HyperTerminal
+ * @param  None.
+ * @retval None.
+ */
 void user_app_run(void)
 {
   uint8_t key = 0;
@@ -72,46 +71,50 @@ void user_app_run(void)
     {
       switch (key)
       {
-        case '1' :
-          user_app_action();
-          break;
-        case 'x':
-          exit = 1;
-          break;
-        default:
-          printf("Invalid Number !\r");
-          break;
+      case '1':
+        user_app_io_action();
+        break;
+      case '2':
+        user_app_chgpwd();
+        break;
+      case 'x':
+        exit = 1;
+        break;
+      default:
+        printf("Invalid Number !\r");
+        break;
       }
       user_app_print_menu();
     }
   }
 }
 /**
-  * @}
-  */
+ * @}
+ */
 
 /** @addtogroup  user_app_Private_Functions
-  * @{
-  */
+ * @{
+ */
 
 /**
-  * @brief  Display the user app Menu choices on HyperTerminal
-  * @param  None.
-  * @retval None.
-  */
+ * @brief  Display the user app Menu choices on HyperTerminal
+ * @param  None.
+ * @retval None.
+ */
 static void user_app_print_menu(void)
 {
   printf("\r\n=============== user app Menu ===================\r\n\n");
   printf("  Toggle LED in secured area ---------------------------- 1\r\n\n");
+  printf("  Change password --------------------------------------- 2\r\n\n");
   printf("  Previous Menu ----------------------------------------- x\r\n\n");
 }
 
 /**
-  * @brief  Display the user app on Terminal
-  * @param  None.
-  * @retval None.
-  */
-static psa_status_t user_app_action(void)
+ * @brief  Display the user app on Terminal
+ * @param  None.
+ * @retval None.
+ */
+static psa_status_t user_app_io_action(void)
 {
   uint8_t pwd[4];
   psa_handle_t handle = PSA_NULL_HANDLE;
@@ -125,7 +128,7 @@ static psa_status_t user_app_action(void)
 
   in_vec[0].base = pwd;
   in_vec[0].len = sizeof(pwd);
-  
+
   handle = psa_connect(TFM_USER_APP_IOCTL_SID,
                        TFM_USER_APP_IOCTL_VERSION);
   if (!PSA_HANDLE_IS_VALID(handle))
@@ -142,5 +145,49 @@ static psa_status_t user_app_action(void)
 }
 
 /**
-  * @}
-  */
+ * @brief  Change the password stored in ITS
+ * @param  None.
+ * @retval None.
+ */
+static psa_status_t user_app_chgpwd(void)
+{
+  uint8_t input[8];
+  psa_handle_t handle = PSA_NULL_HANDLE;
+  psa_status_t status;
+  psa_invec in_vec[1];
+
+  printf("  Key in 4-digit old password and 4-digit new password-----\r\n\n");
+  scanf("%s", input);
+  printf("Input is %s \n", input);
+  printf("sending to secure user app to handle... \r\n");
+
+  in_vec[0].base = input;
+  in_vec[0].len = sizeof(input);
+
+  handle = psa_connect(TFM_USER_APP_CHGPWD_SID,
+                       TFM_USER_APP_CHGPWD_VERSION);
+  if (!PSA_HANDLE_IS_VALID(handle))
+  {
+    return PSA_HANDLE_TO_ERROR(handle);
+  }
+
+  status = psa_call(handle, PSA_IPC_CALL,
+                    in_vec, 1,
+                    NULL, NULL);
+  psa_close(handle);
+
+  if (status != PSA_SUCCESS)
+  {
+    printf("status = %d\r\n", status);
+    printf("Password update is failed. \r\n");
+  }
+  else
+
+    printf("Password update is completed. \r\n");
+
+  return status;
+}
+
+/**
+ * @}
+ */
